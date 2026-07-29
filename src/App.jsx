81b902c30816c5a8ff034ad3b9a8cbea2a995d19
Rebecca Dias <beccadias9@gmail.com>
2026-07-29 16:04:18 -0300

    style: remove comentarios do codigo CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FiltroCategorias from "./componentes/FiltroCategorias/FiltroCategorias.jsx";
+
+function App() {
+
+    return (
+        <>
+            <FiltroCategorias/>
+        </>
+    );
+}
+
+export default App
