6267a155314c5ac0363a45d82fd918eb6557a16e
Rebecca <beccadias9@gmail.com>
2026-07-29 14:43:42 -0300

    melhoria: ajusta responsividade do footer

--- src/components/Footer/FooterBottom.jsx
+++ src/components/Footer/FooterBottom.jsx
@@ -4,9 +4,9 @@ function FooterBottom() {
             <p className="copyright ">© 2026 Pitada de Sal. Todos os direitos reservados.</p>
 
             <ul className="links">
-                <li className="link "><a>Privacidade</a></li>
-                <li className="link "><a>Termos</a></li>
-                <li className="link "><a>Cookies</a></li>
+                <li className="link"><a>Privacidade</a></li>
+                <li className="link"><a>Termos</a></li>
+                <li className="link"><a>Cookies</a></li>
             </ul>
         </div>
     )
