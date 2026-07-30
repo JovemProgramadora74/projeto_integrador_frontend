34a381eb05d79766239abf3d55484599d7a04498
Laura <sanchesdasilvalaura@gmail.com>
2026-07-30 15:59:42 -0300

    feat:hero-section

---
+++ src/components/Hero/HeroText.jsx
@@ -0,0 +1,18 @@
+function HeroText() {
+    return (
+        <div  className="hero-text">
+            <h1 className="titulo">
+                Receitas que <br/> nutrem seu corpo <br/> e sua rotina
+            </h1>
+            <p className="descricao">
+                Práticas, saudáveis e deliciosas para <br/> cada estilo de vida.
+            </p>
+            <button className="btn-explorar">
+                Explorar receitas →
+            </button>
+
+        </div>
+    )
+}
+
+export default HeroText
