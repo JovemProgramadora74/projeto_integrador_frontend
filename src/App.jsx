63db984c99d37b739874f6823d26d45fa8d5278f
Yatra S Venzi <y.venzi@outlook.com>
2026-08-03 14:14:25 -0300

    cria barra de navegacao com base no modelo visual fornecido (#10)
    * cria barra de navegacao com base no modelo visual fornecido
    
    * apaga pastas erradas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css"
+import './App.css'
 
 function App() {
-    return (
-        <>
-        </>
-    );
+
+    return (<>
+            <h1>Olá, mundo</h1>
+        </>)
 }
 
-export default App;
+export default App
