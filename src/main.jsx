be4049db6a6374910f1aa85decde61ab6903ac26
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-08-03 16:08:57 -0300

    cria a base da criação de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/*Adicionar as rotas aqui abaixo */}
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
