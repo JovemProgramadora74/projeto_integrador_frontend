20a2dc231f90dad0b5677eeeee9b5a86265edf50
NandaFerreira211 <mariafernandasantosferreira8@gmail.com>
2026-08-04 13:23:59 -0300

    Indexa pagina de login no main.jsx (#27)

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css'
+import Login from "./pages/Login/Login.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
                 {/*Adicionar as rotas aqui abaixo */}
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
