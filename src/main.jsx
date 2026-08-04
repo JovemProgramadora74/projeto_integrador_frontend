5eb0f0ba57b83c65baa772c075cb994bd1382cdd
Isabela Magella <isabelamagella@gmail.com>
2026-08-04 13:39:54 -0300

    feat: cria página Index

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css'
+import Index from "./pages/Index/Index.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                {/*Adicionar as rotas aqui abaixo */}
+                <Route path="/" element={<Index />} />
             </Routes>
         </BrowserRouter>
-    </StrictMode>,
+    </StrictMode>
 )
