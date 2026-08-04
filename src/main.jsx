1caf1cfa1f14c642d40b8f30161393bc68251646
Rebeca <rebecabarella13@gamil.com>
2026-08-04 14:31:17 -0300

    Cria componente e rota de Cadastro de Usuários

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './index.css'
+import Cadastro from "./pages/Cadastro/Cadastro.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
                 {/*Adicionar as rotas aqui abaixo */}
+                <Route path="/cadastro" element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
