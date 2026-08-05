b89ec8c7c0a0a18a90c4b8ede1d03c932420301c
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-08-05 13:47:39 -0300

    estiliza códigos e corrige o CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
+import './global.css'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import './index.css'
-import Cadastro from "./pages/Cadastro/Cadastro.jsx";
-import Index from "./pages/Index/Index.jsx";
-import Login from "./pages/Login/Login.jsx";
-import ListagemChefs from "./pages/ListagemChefs/ListagemChefs.jsx";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Index from './pages/Index/Index.jsx'
+import Cadastro from './pages/Cadastro/Cadastro.jsx'
+import Login from './pages/Login/Login.jsx'
+import ListagemChefs from './pages/ListagemChefs/ListagemChefs.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                {/*Adicionar as rotas aqui abaixo */}
-                <Route path="/cadastro" element={<Cadastro />} />
-                <Route path="/" element={<Index />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/meus-chefes" element={<ListagemChefs />} />
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
-)
+createRoot(document.getElementById('root')).render(<StrictMode>
+    <BrowserRouter>
+        <Routes>
+            {/*Adicionar as rotas aqui abaixo */}
+            <Route path='/' element={<Index/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/cadastro' element={<Cadastro/>}/>
+            <Route path='/meus-chefes' element={<ListagemChefs/>}/>
+        </Routes>
+    </BrowserRouter>
+</StrictMode>)
